393b471a573b11634af9eb14af2f5072d00ae397
Santiago Pirela <s17manutd@gmail.com>
2022-07-10 21:51:35 -0400

    :sparkles: messages module done

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,39 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
   listMessages(@Query('order') order: string, @Query('limit') limit: number) {
-    return `list ordered in ${order} order. limit: ${limit}`;
+    console.log(`list ordered in ${order} order. limit: ${limit}`);
+
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
-    return 'post';
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return `Queried id: ${id}`;
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException(`Message with id: ${id} not found`);
+    }
+
+    return message;
   }
 }
