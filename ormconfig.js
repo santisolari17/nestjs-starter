a3194096185585359a49c0b996980fd9a5ed9787
Santiago Pirela <s17manutd@gmail.com>
2022-11-07 10:26:28 -0300

    heroku ready

--- ormconfig.js
+++ ormconfig.js
@@ -31,7 +31,17 @@ function setDbCOnfigByEnvironment(environment) {
           entities: ['**/*.entity.js'],
         });
       },
-      [environments.production]: () => 100,
+      [environments.production]: () => {
+        Object.assign(dbConfig, {
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          migrationsRun: true,
+          entities: ['**/*.entity.js'],
+          ssl: {
+            recjectUnauthorized: false,
+          },
+        });
+      },
       default: () => {
         throw new Error(`Error at dbConfig setup: unknown environment [${environment}]`);
       },
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   // app.use(cookieSession({ keys: ['encryptionString'] })); // set in AppModule.configure()
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));  // set in appmodule
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
